1e40aaaa86e4b9202ada4686365c2e8503f2b489
Mostafa Zaki <mostafazke@gmail.com>
2023-01-22 23:34:41 +0100

    add app assets

--- next.config.js
+++ next.config.js
@@ -1,8 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'a.espncdn.com',
+        port: '',
+        pathname: '/i/teamlogos/**/**',
+      },
+    ],
+    formats: ['image/avif', 'image/webp'],
+  },
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
